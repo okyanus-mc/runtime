90cbcf269f553a14dfead94f1b0128180a065f6b
hasan demirtaş <hsndmrts_@outlook.com>
2019-07-01 20:15:21 +0300

    Update ServerImpl.java

--- src/main/java/club/issizler/okyanus/api/ServerImpl.java
+++ src/main/java/club/issizler/okyanus/api/ServerImpl.java
@@ -77,7 +77,7 @@ public class ServerImpl implements Server {
 
     @Override
     public EventRegistry getEventRegistry() {
-        return eventdRegistry;
+        return eventRegistry;
     }
 
     @Override
