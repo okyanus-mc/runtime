aaa72e0a048e54700bf9b9777e10c0ce38fa9439
hasan demirtaş <hsndmrts_@outlook.com>
2019-07-01 20:02:43 +0300

    Update ModRegistryImpl.java

--- src/main/java/club/issizler/okyanus/api/registry/ModRegistryImpl.java
+++ src/main/java/club/issizler/okyanus/api/registry/ModRegistryImpl.java
@@ -4,6 +4,7 @@ import club.issizler.okyanus.api.ModRegistry;
 import club.issizler.okyanus.api.cmdnew.Command;
 import club.issizler.okyanus.api.event.Event;
 import club.issizler.okyanus.api.event.EventHandler;
+import org.cactoos.list.ListOf;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +12,16 @@ import java.util.List;
 public class ModRegistryImpl implements ModRegistry {
 
     private final List<Command> commandList = new ArrayList<>();
-    private final List<Event> eventList = new ArrayList<>();
+    private final List<EventHandler> eventList = new ArrayList<>();
 
     @Override
     public void registerCommand(Command... commands) {
-
+        commandList.addAll(new ListOf<>(commands));
     }
 
     @Override
     public void registerEvent(EventHandler... events) {
-
+        eventList.addAll(new ListOf<>(events));
     }
 
 }
