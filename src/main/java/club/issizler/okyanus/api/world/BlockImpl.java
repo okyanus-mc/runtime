1591a8fd4f5f503bd2d1c1d5cd05eef2cc3f3e2a
Ecmel Berk Canlıer <me@ecmelberk.com>
2019-07-03 18:37:14 +0300

    Add location getter to Block

--- src/main/java/club/issizler/okyanus/api/world/BlockImpl.java
+++ src/main/java/club/issizler/okyanus/api/world/BlockImpl.java
@@ -1,13 +1,21 @@
 package club.issizler.okyanus.api.world;
 
+import club.issizler.okyanus.api.math.Vec3d;
 import net.minecraft.block.BlockState;
 
 public class BlockImpl implements Block {
 
     private final BlockState block;
+    private final Vec3d pos;
 
-    BlockImpl(BlockState block) {
+    BlockImpl(BlockState block, Vec3d pos) {
         this.block = block;
+        this.pos = pos;
+    }
+
+    @Override
+    public Vec3d getLocation() {
+        return pos;
     }
 
 }
