4bbac0142c7dcf464c6cea7060642f759039e47e
hasan demirtaş <hsndmrts_@outlook.com>
2019-07-10 13:11:25 +0300

    Update EntityImpl.java

--- src/main/java/club/issizler/okyanus/api/entity/EntityImpl.java
+++ src/main/java/club/issizler/okyanus/api/entity/EntityImpl.java
@@ -71,7 +71,9 @@ public class EntityImpl implements Entity {
 
     @Override
     public boolean equals(Object o) {
-        return entity.equals(o);
-    }
+        if (!(o instanceof EntityImpl))
+            return false;
 
+        return ((EntityImpl) o).entity.getEntityId() == entity.getEntityId();
+    }
 }
